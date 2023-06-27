945cba31a3d6ff3f28ab196955a5f5c80e9f717f
dariuszszmycinski <68079317+dariuszszmycinski@users.noreply.github.com>
2023-06-27 08:20:45 +0200

    Add files via upload

---
+++ src/main/java/dasz/Kata.java
@@ -0,0 +1,10 @@
+package dasz;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static double findUniq(double arr[]) {
+        Arrays.sort(arr);
+        return arr[0]!=arr[1]?arr[0]:arr[arr.length-1];
+    }
+}
